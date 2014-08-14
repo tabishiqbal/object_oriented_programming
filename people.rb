d2ff86a50fed5333e49f55641a3a5de3933e754c
Tabish <tabishiqbal@outlook.com>
2014-08-14 13:37:20 -0400

    Added Parent, Student and Instructor class and created new instance of instructor whos name is chris

--- people.rb
+++ people.rb
@@ -1,5 +1,27 @@
-class Student
+# Parent class
+class Parent
+  def initialize(name)
+    @name = name
+  end
+
+  def greeting
+    "Hi, my name is #{@name}."
+  end
 end
 
-class Instructor
+# Student Class
+class Student
+	def learn
+		 "I get it"
+	end
 end
+
+# Instructor Class
+class Instructor
+  def teach
+    "Everything in Ruby is an Object"
+  end
+end
+
+c = Parent.new("Chris")
+puts c.greeting
