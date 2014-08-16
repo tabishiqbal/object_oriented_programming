ed09bb561b8f558ae6569f1fed8c10524e5e0dfc
Tabish <tabishiqbal@outlook.com>
2014-08-16 12:39:01 -0400

    Fixed issue with not getting proper values

--- rover.rb
+++ rover.rb
@@ -3,59 +3,61 @@ class Rover
   attr_accessor :x, :y, :direction
 
     def initialize(x, y, direction)
-      @x = x
-      @y = y
-      @direction = direction
+        @x = x
+        @y = y
+        @direction = direction
     end
 
 
     def current_position
-     puts "#{@x}, #{@y}, #{@direction}"
+        puts "#{@x}, #{@y}, #{@direction}"
     end
 
 
     def rotate(instructions)
      
-      if instructions == "L" && @direction == "N"
-        @direction = "W"
-      end
-      if instructions == "L" && @direction == "W"
-        @direction = "S"
-      end
-      if instructions == "L" && @direction == "S"
-        @direction = "E"
-      end
-      if instructions == "L" && @direction == "E"
-        @direction = "N"
-      end
-      if instructions == "R" && @direction == "N"
-        @direction = "E"
-      end
-      if instructions == "R" && @direction == "E"
-        @direction = "S"
-      end
-      if instructions == "R" && @direction == "S"
-        @direction = "W"
-      end
-      if instructions == "R" && @direction == "W"
-        @direction = "N"
-      end
+        if instructions == "L" && @direction == "N"
+          @direction = "W"
+            
+        elsif instructions == "L" && @direction == "W"
+          @direction = "S"
+        
+        elsif instructions == "L" && @direction == "S"
+          @direction = "E"
+        
+        elsif instructions == "L" && @direction == "E"
+          @direction = "N"
+        
+        elsif instructions == "R" && @direction == "N"
+          @direction = "E"
+        
+        elsif instructions == "R" && @direction == "E"
+          @direction = "S"
+        
+        elsif instructions == "R" && @direction == "S"
+          @direction = "W"
+        
+        else instructions == "R" && @direction == "W"
+          @direction = "N"
+        end
 
     end
 
 
     def move
-      if @direction == "N"
-        @y += 1
-      elsif @direction == "W"
-        @x -= 1
-      elsif @direction == "E"
-        @x += 1
-      elsif @direction == "S"
-        @y -= 1
-      end 
-    end
 
+      case @direction
+      when "N"
+        @y += 1
+      when "E"
+        @x += 1
+      when "S"
+        @y -= 1
+      when "W"
+        @x -= 1
+      end
+
+    end
 
 end
 
@@ -63,31 +65,32 @@ end
 class UserInput
 
   def self.position
-    puts "Where would you want your rover to move?"
+    puts "Where would you want your rover to land? (x, y, direction)"
     
-    @position = gets.chomp.upcase
+    @position = gets.chomp
     
     @x = @position.split(//)[1].to_i
     @y = @position.split(//)[3].to_i
-    @direction = @position.split(//)[5]
+    @direction = @position.split(//)[5].upcase
     
     @rover_one = Rover.new(@x, @y, @direction)
   end
 
   def self.order
-    puts "Please input L and R instructions"
+    puts "use 'L', 'R', and 'M' commands to move your rover"
     @order = gets.chomp.upcase
-    # puts "#{@order}"
-    @order.split(//).each do |bob|
-      if bob == "M" 
+    @order.split(//).each do |x|
+      if x == "M" 
         @rover_one.move
       else
-        @rover_one.rotate(bob)
+        @rover_one.rotate(x)
       end
     end
     @rover_one.current_position
   end
 end
+
+
 
 UserInput.position
 UserInput.order
